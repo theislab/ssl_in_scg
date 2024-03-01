3e0145cdaef5ba0eecf23bf4b15ae6c74502acb2
richtertill <till-richter@gmx.de>
2024-03-01 16:47:29 +0100

    Cleanup repo and add explanations

--- self_supervision/models/contrastive/bt.py
+++ self_supervision/models/contrastive/bt.py
@@ -159,20 +159,6 @@ class LARS(optim.Optimizer):
                 p.add_(mu, alpha=-g["lr"])
 
 
-"""
-class GaussianBlur(object):
-    def __init__(self, p):
-        self.p = p
-
-    def __call__(self, img):
-        if random.random() < self.p:
-            sigma = random.random() * 1.9 + 0.1
-            return img.filter(ImageFilter.GaussianBlur(sigma))
-        else:
-            return img
-"""
-
-
 class GaussianBlur(object):
     def __init__(self, p: float):
         """
